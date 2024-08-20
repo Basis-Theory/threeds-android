d470b6aad96d97f0b59fd907e50641b572f22b1f
kevin <kevin@basistheory.com>
2024-08-19 20:36:40 -0600

    refactor: adds 3ds service builder
    feat: fetch ravelin sdk key from api

--- example/src/main/java/com/basistheory/threeds/example/view/three_ds/ThreeDs.kt
+++ example/src/main/java/com/basistheory/threeds/example/view/three_ds/ThreeDs.kt
@@ -6,26 +6,33 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.basistheory.threeds.example.databinding.FragmentThreeDsBinding
 import com.basistheory.threeds.example.viewmodel.ThreeDsViewModel
+import com.basistheory.threeds.service.Region
 import com.basistheory.threeds.service.ThreeDsService
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class ThreeDs : Fragment() {
-    private val binding: FragmentThreeDsBinding  by lazy {
+    private val binding: FragmentThreeDsBinding by lazy {
         FragmentThreeDsBinding.inflate(layoutInflater)
     }
 
-    private val threeDsService = ThreeDsService()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val threeDsService = ThreeDsService
+            .Builder()
+            .withApiKey("btApiKey")
+            .withApplicationContext(requireContext().applicationContext)
+            .withRegion(Region.EU)
+            .build()
+
         binding.viewModel = ThreeDsViewModel(threeDsService);
 
 
         this.lifecycleScope.launch {
             withContext(Dispatchers.IO) {
-                binding.viewModel!!.initialize(requireContext().applicationContext)
+                binding.viewModel!!.initialize()
             }
         }
     }
--- example/src/main/java/com/basistheory/threeds/example/viewmodel/ThreeDsViewModel.kt
+++ example/src/main/java/com/basistheory/threeds/example/viewmodel/ThreeDsViewModel.kt
@@ -1,15 +1,14 @@
 package com.basistheory.threeds.example.viewmodel
 
-import android.content.Context
 import androidx.lifecycle.ViewModel
 import com.basistheory.threeds.service.ThreeDsService
 
 open class ThreeDsViewModel(private val threeDsService: ThreeDsService): ViewModel() {
-    suspend fun initialize(context: Context) {
+    suspend fun initialize() {
         runCatching {
-            threeDsService.initialize(context, null, null)
+            threeDsService.initialize()
         }.onFailure {
-            throw error("Failed to initialize service in view model: ${it.localizedMessage}")
+            throw it
         }
     }
 }
