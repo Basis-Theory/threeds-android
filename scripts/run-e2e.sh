ab9abf7b8be434c91827ad0079683954422cfe77
kevin <kevin@basistheory.com>
2024-09-24 11:24:18 -0600

    disable video recording and screenshots

--- scripts/run-e2e.sh
+++ scripts/run-e2e.sh
@@ -2,8 +2,8 @@
 set -x
 TMP_FILE=_fail_proccess
 # Start video recording
-$ANDROID_HOME/platform-tools/adb shell screenrecord /sdcard/video_record.mp4 & echo $! > video_record.pid
-sleep 3
+# $ANDROID_HOME/platform-tools/adb shell screenrecord /sdcard/video_record.mp4 & echo $! > video_record.pid
+# sleep 3
 APP_ID=com.basistheory.threeds.example
 npx envinfo
 adb devices
@@ -18,16 +18,16 @@ adb install example/build/outputs/apk/debug/example-debug.apk
 # Save logs to upload as artifact
 adb logcat -d > emulator_logcat.txt
 # Stop video recording
-kill -SIGINT "$(cat video_record.pid)"
-sleep 3
-rm -rf video_record.pid
+# kill -SIGINT "$(cat video_record.pid)"
+# sleep 3
+# rm -rf video_record.pid
 # Grab screenshots
-$ANDROID_HOME/platform-tools/adb shell screencap -p /sdcard/last_img.png
-$ANDROID_HOME/platform-tools/adb pull /sdcard/last_img.png
-$ANDROID_HOME/platform-tools/adb pull /sdcard/video_record.mp4
-$ANDROID_HOME/platform-tools/adb shell rm /sdcard/video_record.mp4
+# $ANDROID_HOME/platform-tools/adb shell screencap -p /sdcard/last_img.png
+# $ANDROID_HOME/platform-tools/adb pull /sdcard/last_img.png
+# $ANDROID_HOME/platform-tools/adb pull /sdcard/video_record.mp4
+# $ANDROID_HOME/platform-tools/adb shell rm /sdcard/video_record.mp4
 # Debug
-echo "::group::Maestro hierarchy"
+# echo "::group::Maestro hierarchy"
 #     ^^^^^^^^ see: https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#grouping-log-lines
-$HOME/.maestro/bin/maestro hierarchy
-echo "::endgroup::"
+# $HOME/.maestro/bin/maestro hierarchy
+# echo "::endgroup::"
